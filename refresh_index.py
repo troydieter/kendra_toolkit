2a15bdb5509cda733733e9eb77ce9ec063aef3b6
Troy Dieter <troydieter@gmail.com>
2022-10-31 21:09:46 -0400

    pseudo code

--- refresh_index.py
+++ refresh_index.py
@@ -5,13 +5,14 @@ from botocore.exceptions import ClientError
 kendra = boto3.client("kendra")
 
 # Set the Amazon Kendra Index
-INDEX_NAME = 'example'
+INDEX_ID = 'XX'
 
 try:
     response = kendra.list_data_sources(
-        IndexId=INDEX_NAME,
+        IndexId=INDEX_ID,
         MaxResults=10
     )
+    print(response)
 
 except  ClientError as e:
         print("%s" % e)
