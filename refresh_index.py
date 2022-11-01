75e7951bd5a92d00e759a4901e3696082067f790
Troy Dieter <troydieter@gmail.com>
2022-10-31 21:02:27 -0400

    testing list of ds

--- refresh_index.py
+++ refresh_index.py
@@ -1,8 +1,17 @@
 import boto3
+from botocore.exceptions import ClientError
 
-# Pseudo code
+# Establish the Amazon Kendra Client
 kendra = boto3.client("kendra")
 
+# Set the Amazon Kendra Index
+INDEX_NAME = 'example'
 
-def refresh_index(event, context):
-    print(event)
+try:
+    response = kendra.list_data_sources(
+        IndexId=INDEX_NAME,
+        MaxResults=10
+    )
+
+except  ClientError as e:
+        print("%s" % e)
